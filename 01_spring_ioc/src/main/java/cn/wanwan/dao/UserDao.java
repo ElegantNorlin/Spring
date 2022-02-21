0128b575ff8792323c97ef61b03854563ee0a69c
万万 <15863465252@163.com>
2022-02-21 15:42:15 +0800

    first commit

---
+++ 01_spring_ioc/src/main/java/cn/wanwan/dao/UserDao.java
@@ -0,0 +1,5 @@
+package cn.wanwan.dao;
+
+public interface UserDao {
+    public void save();
+}
---
+++ 01_spring_ioc/src/main/java/cn/wanwan/dao/impl/UserDaoImpl.java
@@ -0,0 +1,10 @@
+package cn.wanwan.dao.impl;
+
+import cn.wanwan.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public void save() {
+        System.out.println("save running...");
+    }
+}
