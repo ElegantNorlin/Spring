32cf69ba559a94efa188809b8433ac21ab4e51e0
万万 <15863465252@163.com>
2022-02-23 11:19:58 +0800

    add annotations

--- itheima_spring_test/src/main/java/com/itheima/controller/RoleController.java
+++ itheima_spring_test/src/main/java/com/itheima/controller/RoleController.java
@@ -12,16 +12,19 @@ import java.util.List;
 @RequestMapping("/role")
 @Controller
 public class RoleController {
-
+    //@Autowired:注入roleService对象
     @Autowired
     private RoleService roleService;
 
+    //@RequestMapping("/save"):设置save方法的访问路径
+    //save(Role role)方法中的Role对象是由SpringMVC自动封装的，表单中的属性值和Role对象的属性值一致，框架自动封装该对象
     @RequestMapping("/save")
     public String save(Role role){
         roleService.save(role);
         return "redirect:/role/list";
     }
 
+    //@RequestMapping("/list"):设置list方法的访问路径
     @RequestMapping("/list")
     public ModelAndView list(){
         ModelAndView modelAndView = new ModelAndView();
